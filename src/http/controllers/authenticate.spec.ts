4028d05fff83da0e60cf3378d1b71659798a5992
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-07-12 19:46:53 -0300

    test(auth): fluxo de autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,4 +1,32 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-test("ok", () => {});
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password: "123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "johndoe@email.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty("token");
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
