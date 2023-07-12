873387772e4a844ba4aaddb12c94d40ea35f21f4
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-07-12 20:05:52 -0300

    test(profile): busca do perfil do usuário

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,41 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+
+describe("Profile (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get user profile", async () => {
+    await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password: "123456",
+    });
+
+    const authResponse = await request(app.server).post("/sessions").send({
+      email: "johndoe@email.com",
+      password: "123456",
+    });
+
+    const { token } = authResponse.body;
+
+    const profileResponse = await request(app.server)
+      .get("/me")
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(profileResponse.statusCode).toEqual(200);
+    // expect(profileResponse.body).toHaveProperty("token");
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: "johndoe@email.com",
+      }),
+    );
+  });
+});
