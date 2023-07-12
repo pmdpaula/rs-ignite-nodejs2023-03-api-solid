b0ad943e1d003153a9432a5e827d5924a0118ef4
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-07-12 18:59:54 -0300

    test(config): configuração para execução de teste e2e
    Criaremos um schema no banco de dados para cada test que for feito. Isto serve para que os dados sejam sempre limpos em cada teste e2e.

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,4 +1,23 @@
+import "dotenv/config";
+
+import { randomUUID } from "crypto";
 import { Environment } from "vitest";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
@@ -6,12 +25,21 @@ export default <Environment>{
     // Setup the environment
     // This function will be called once before all tests
     // and before the server is started.
-    console.log("Executou");
+
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync("npx prisma migrate deploy");
 
     // Return the environment object
     return {
       async teardown() {
-        console.log("Teardown");
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`); //`
+        await prisma.$disconnect();
+
+        //`
       },
     };
   },
