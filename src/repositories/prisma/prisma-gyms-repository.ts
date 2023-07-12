6c77a7271ccea4ee3a8aef60a83153a25dea46d2
Pedro Machado de Paula <pmdpaula@gmail.com>
2023-07-12 13:24:30 -0300

    feat(repository): implantação dos métodos do repositório gyms

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,47 @@
+import { Gym, Prisma } from "@prisma/client";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
+
+export class PrismaGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    });
+
+    return gym;
+  }
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: "insensitive",
+        },
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
+
+    return gyms;
+  }
+
+  // uso do raw query para fazer a query de distância
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `;
+
+    return gyms;
+  }
+}
